83aff415b2fd45a9fcd4d799f21e4e1b9f864b49
supervueman <supervueman@gmail.com>
2021-11-05 20:51:22 +0400

    Add new test

---
+++ src/components/Greeting/tests/unit/greeting.spec.js
@@ -0,0 +1,9 @@
+import { mount } from '@vue/test-utils'
+import Greeting from '@/components/Greeting/Greeting.vue'
+
+describe('Greeting.vue', () => {
+  it('отрисовывает приветствие', () => {
+    const wrapper = mount(Greeting)
+    expect(wrapper.text()).toMatch("Vue и TDD")
+  })
+})
