e6f8bfcd88353d3a9d6665cec4ec05e46f8c5281
supervueman <supervueman@gmail.com>
2021-11-05 20:36:57 +0400

    Test test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,30 @@
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
+const factory = (values = {}, props = {}) => {
+  return shallowMount(HelloWorld, {
+    propsData: {
+      ...props
+    },
+    data () {
+      return {
+        ...values
+      }
+    }
+  })
+}
+
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+    const msg = 'Hello world'
+    const wrapper = factory({}, { msg })
+
+    expect(wrapper.find('.message').text()).toMatch(msg)
+  })
+
+  it('renders props.msg when no passed', () => {
+    const wrapper = factory()
+
+    expect(wrapper.find('.warning').text()).toEqual('No text')
   })
 })
