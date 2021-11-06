04172b919ecf954cf2a1b55deafa3e31109d56aa
supervueman <supervueman@gmail.com>
2021-11-06 23:09:30 +0400

    Fix

--- src/components/Bilingual/tests/unit/bilingual.spec.js
+++ src/components/Bilingual/tests/unit/bilingual.spec.js
@@ -1,5 +1,10 @@
-import { mount } from '@vue/test-utils'
+import { mount, config } from '@vue/test-utils'
 import Bilingual from '@/components/Bilingual/Bilingual.vue'
+import translations from '../../translations.js'
+
+const locale = 'en'
+
+config.mocks.$t = (msg) => translations[locale][msg]
 
 describe('Bilingual.vue', () => {
   it('Успешно отрисовывается', () => {
