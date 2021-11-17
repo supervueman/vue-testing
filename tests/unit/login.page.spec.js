34d4ecc44b003976c70e03d38c4c5321749f2f61
supervueman <supervueman@gmail.com>
2021-11-17 21:09:37 +0400

    Add test for find component in page login

--- tests/unit/login.page.spec.js
+++ tests/unit/login.page.spec.js
@@ -1,8 +1,9 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import Login from '@/views/Login.vue'
+import LoginForm from '@/components/LoginForm.vue'
 
 const factory = (data) => {
-  return shallowMount(Login, {
+  return mount(Login, {
     ...data
   })
 }
@@ -16,5 +17,15 @@ describe('Logion page', () => {
     })
 
     expect(wrapper.find('.pagetitle').text()).toBe('Login')
+  })
+
+  it('Render LoginForm component', () => {
+    const wrapper = factory({
+      data: {
+        title: 'Login'
+      }
+    })
+
+    expect(wrapper.findComponent(LoginForm).exists()).toBe(true)
   })
 })
